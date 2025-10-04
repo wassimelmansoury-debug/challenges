f38a5d74f222bb381a7a2f50f5a5894a78c05a1c
wassimelmansoury-debug <wassimelmansoury@gmail.com>
2025-10-04 17:34:22 +0100

    ex_1.js

--- ex_fonction/ex_1.js
+++ ex_fonction/ex_1.js
@@ -1,16 +1,7 @@
-// function remplir(){
-// let num_variable1=Number(prompt("entrez le nombre 1:");
-// let num_variable2=Number(prompt("entrez le nombre 2:");
-// }
-
-function add(a,b){
-    return a + b
+function sommeTableau(tab) {
+  let somme = 0;
+  for (let i = 0; i < tab.length; i++) {
+    somme += tab[i];
+  }
+  return somme;
 }
-function menu(){
-    let num = "";
-let i = 0;
-do {
-  num += i + "\n";
-  i++;
-}
-while (i < 5);
